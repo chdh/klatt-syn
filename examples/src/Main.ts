d05b031222d90dff525201a10daea4247aa3b5d0
Christian d'Heureuse <chdh@inventec.ch>
2026-01-26 00:03:15 +0100

    Minor update

--- examples/src/Main.ts
+++ examples/src/Main.ts
@@ -1,11 +1,11 @@
 // Example program for the KlattSyn package.
 
-import * as Fs from "node:fs";
 import * as KlattSyn from "klatt-syn";
 import * as WindowFunctions from "dsp-collection/signal/WindowFunctions";
 import * as WavFileEncoder from "wav-file-encoder";
+import * as Fs from "node:fs";
 
-export function fadeAudioSignalInPlace (samples: Float64Array, fadeMargin: number) {
+function fadeAudioSignalInPlace (samples: Float64Array, fadeMargin: number) {
    const windowFunction = WindowFunctions.hannWindow;
    const d = Math.min(samples.length, 2 * fadeMargin);
    for (let i = 0; i < d / 2; i++) {
@@ -17,18 +17,22 @@ export function fadeAudioSignalInPlace (samples: Float64Array, fadeMargin: numbe
 
 function main() {
 
+   // Generate vowel sound signal.
    const sampleRate = 44100;
    const glottalSourceType = KlattSyn.GlottalSourceType.impulsive;
    const mainParms: KlattSyn.MainParms = {sampleRate, glottalSourceType};
-   const frameParmsA = [KlattSyn.demoFrameParms];
-   const signalSamples = KlattSyn.generateSound(mainParms, frameParmsA);
+   const frameParms = KlattSyn.demoFrameParms;
+   const signalSamples = KlattSyn.generateSound(mainParms, [frameParms]);
 
-   const fadingDuration = 0.05;
+   // Apply fade-in / fade-out.
+   const fadingDuration = 0.05;                                                // 50 ms fade-in and fade-out
    fadeAudioSignalInPlace(signalSamples, fadingDuration * sampleRate);
 
+   // Write output to WAV file.
    const wavFileData = WavFileEncoder.encodeWavFileFromArrays([signalSamples], sampleRate, WavFileEncoder.WavFileType.float32);
    const outputFileName = "example.wav";
    Fs.writeFileSync(outputFileName, Buffer.from(wavFileData));
+
    console.log(`Audio data written to file "${outputFileName}".`);
 }
 
